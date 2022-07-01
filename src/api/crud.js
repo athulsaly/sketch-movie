cc6f3bd4c2595dee79641b835309ff109631b3b8
athulsaly <athulsaly@gmail.com>
2022-07-01 13:18:39 +0530

    search update

--- src/api/crud.js
+++ src/api/crud.js
@@ -1,28 +1,18 @@
 //
 import axios from "axios";
 
-var api_id = "8c5825603819c6565d47f38f78f6d76d";
 
-const axiosInstance = axios.create({
-  baseURL: `https://api.themoviedb.org/3/movie/popular?api_key=${api_id}&language=en-US`,
-  timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-    accept: "application/json",
-  },
-});
-
-// GET ALL Popular
-const getAllPopular = async (padgeNum) => {
+// GET Searched
+const getSearched = async (search) => {
   let data = [];
-  let fetch_api_url = `&page=${padgeNum}`;
 
-  let response = await axiosInstance.get(fetch_api_url);
+
+  let response = await axios.get(  `https://api.themoviedb.org/3/search/movie?api_key=8c5825603819c6565d47f38f78f6d76d&language=en-US&query=${search}&page=1&include_adult=true`);
   if (response) {
     data = response.data;
-    return data;
+    return data.results[0];
   }
-  return data;
+  return data.results[0];
 };
 
-export { getAllPopular };
+export { getSearched };
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -3,13 +3,14 @@ import { useNavigate, useLocation } from "react-router-dom"; //state-management
 import "./movies.css";
 import axios from "axios";
 import Header from "../components/header/Header";
+import BackButton from "../assets/back.png";
 
 const Movies = () => {
   const navigate = useNavigate();
-  /*   const [data, setData] = useState([]);
+  const [datax, setData] = useState([]);
   const { state } = useLocation();
   const { movie_id } = state;
-  console.log(movie_id)
+
   useEffect(() => {
     axios
       .get(
@@ -17,19 +18,38 @@ const Movies = () => {
       )
 
       .then((data) => {
-        console.log(data.data)
-        setData(data.data.results);
+   
+        setData(data.data);
       });
-  });
-  console.log(data);
- */
+  }, []);
+
+
   return (
     <>
-      <Header player={true}/>
-      <div>
-        <h1>Contact Page</h1>
-        <br />
-        <button onClick={() => navigate(-1)}>Go Back</button>
+      <Header player={true}  />
+      <div className="mainx">
+        <div className="mainx__left" > 
+        <div className="top">
+          <button onClick={() => navigate(-1)}>
+  
+            <img src={BackButton} alt="Back" />
+          </button>
+          </div>
+          <h1>{datax.original_title}</h1>
+          <p>Rating : &nbsp;{datax.vote_average / 2}/5</p>
+          <p id="overview">{datax.overview}</p>
+          <div className="release__date">
+          <p>Release Date</p>
+          <p id="release">{datax.release_date}</p>
+          </div>
+          <div className="release__date">
+          <p>Original Language</p>
+          <p id="release">{datax.original_language ? datax.original_language.toUpperCase() : ""}</p>
+          </div>
+        </div>
+        {<div className="main__right"   >
+          <img src={`https://image.tmdb.org/t/p/w500/${datax.backdrop_path}`} alt={datax.original_title} />
+        </div>}
       </div>
     </>
   );
